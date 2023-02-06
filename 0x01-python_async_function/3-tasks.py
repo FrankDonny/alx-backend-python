fddebf02d20607bb501079365f284e00c68baa59
frankdonny <arkohfrank1@gmail.com>
2023-02-06 21:41:44 +0000

    0x01. Lockboxes

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,5 +7,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> Any:
-    task: Any = asyncio.create_task(wait_random())
+    task: Any = asyncio.create_task(wait_random(max_delay))
     return task
