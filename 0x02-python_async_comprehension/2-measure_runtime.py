757789b9e028019ec9c2b4363cedbd6d64c02e6a
frankdonny <arkohfrank1@gmail.com>
2023-02-08 00:43:11 +0000

    0x02. Python - Async Comprehension

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 """module two for async generators"""
 import asyncio
+import time
 from typing import List
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime():
+    """function for asynccio.gather"""
+    start = time.time()
+    results = await asyncio.gather(*(async_comprehension() for i in range(4)))
+    totalTime = time.time() - start
+    return totalTime
