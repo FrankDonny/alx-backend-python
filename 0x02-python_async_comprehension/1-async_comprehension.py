5a329d0a0bdfb87c9b5f9c11db26a9a406a57bfb
frankdonny <arkohfrank1@gmail.com>
2023-02-08 00:27:12 +0000

    0x02. Python - Async Comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,9 +9,3 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     """using an async comprehension"""
     return [num async for num in async_generator()]
-
-
-async def main():
-    print(await async_comprehension())
-
-asyncio.run(main())
---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+"""module two for async generators"""
+import asyncio
+from typing import List
