e4827939d2db0fa1ed6b20e99ccc2fc0ad03ffea
frankdonny <arkohfrank1@gmail.com>
2023-02-27 02:01:30 +0000

    0x03. Unittests and Integration Tests

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""testing for test_utils"""
+
+import unittest
+
+from parameterized import parameterized, parameterized_class
+
+import utils
+
+
+class TestAccessNestedMap(unittest.TestCase):
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, in1, in2, output):
+        self.assertEqual(utils.access_nested_map(in1, in2), output)
+
+    @parameterized.expand([
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
+    ])
+    def test_access_nested_map_exception(self, in2, in3, output):
+        self.assertRaises(output, utils.access_nested_map, in2, in3)
+
+
+if __name__ == "__main__":
+    unittest.main()
